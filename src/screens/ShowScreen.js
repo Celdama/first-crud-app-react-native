a8a106816ef8f16db47880b5b155fdad88026027
Celdama <celdama.dev@gmail.com>
2022-08-29 15:46:06 +0200

    create screens/ShowScreen.js

---
+++ src/screens/ShowScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const ShowScreen = () => {
+  return (
+    <View>
+      <Text>ShowScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ShowScreen;
