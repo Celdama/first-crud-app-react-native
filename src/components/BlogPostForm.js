c499a651aa7a298d0237199369439f8ca63b3aba
Celdama <celdama.dev@gmail.com>
2022-08-30 08:55:55 +0200

    components/BlogPostForm: create

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,9 @@
+import { StyleSheet, View } from 'react-native';
+
+const BlogPostForm = () => {
+  return <View>BlogPostForm</View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default BlogPostForm;
