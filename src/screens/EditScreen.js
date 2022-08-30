26bfdc1b429ed8c15b85ee82ad2d398fb3d9a69f
Celdama <celdama.dev@gmail.com>
2022-08-30 08:12:16 +0200

    screens/EditScreen: add logic to edit blog post inside EditScreen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,12 +1,37 @@
-import { useContext, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useContext, useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+
+import { Context } from '../context/BlogContext';
 
 const EditScreen = ({ navigation }) => {
+  const [title, setTitle] = useState(null);
+  const [content, setContent] = useState(null);
+  const id = navigation.getParam('id');
+  const { state } = useContext(Context);
+
+  useEffect(() => {
+    const blogPost = state.find((blogPost) => blogPost.id === id);
+
+    setTitle(blogPost.title);
+    setContent(blogPost.content);
+  }, [id]);
+
   return (
     <View>
-      <Text style={styles.label}>Enter Title:</Text>
-
-      <Text style={styles.label}>Enter Content:</Text>
+      <Text style={styles.label}>Edit Title:</Text>
+      <TextInput
+        value={title}
+        style={styles.input}
+        onChangeText={(textChange) => setTitle(textChange)}
+      />
+      <Text style={styles.label}>Edit Content:</Text>
+      <TextInput
+        value={content}
+        style={styles.input}
+        onChangeText={(textChange) => setContent(textChange)}
+      />
+      <Button title='Edit Blog Post' onPress={() => console.log('edited')} />
     </View>
   );
 };
